9d7f8a1db10707874adfe60011fb5cf508375e08
MoMoOwO <strangersj@outlook.com>
2020-04-12 22:10:57 +0800

    mongoose更新和删除操作

--- MongoDB+Monogoose相关/02.mongoose_update_delete.js
+++ MongoDB+Monogoose相关/02.mongoose_update_delete.js
@@ -10,24 +10,52 @@ mongoose.connect("mongodb://localhost:27017/eggcms", {
 // 操作 user 表（集合），定义一个 Schema
 // Schema 中的字段需要与数据库中的字段一一对应，并对字段格式进行规定
 var NewsSchema = mongoose.Schema({
-    title: String,
+    title: "string", // 这样表示类型也可以
     author: String,
     pic: String,
     content: String,
-    status: 1
+    status: Number
 });
 
 // 定义数据库模型，操作数据库
 // 通过第三个参数来指定要操作的数据库
-var User = mongoose.model("User", UserSchema, "user"); // 操作第三个参数指定的表
+var News = mongoose.model("News", NewsSchema, "news"); // 操作第三个参数指定的表
 
 // 7. 更新数据
-
-
-User.find({}, (err, doc) => {
+/*
+    第一个参数为查询条件，即查询到要修改的数据
+    第二个参数为修改条件
+*/
+/* console.log("-----update------");
+News.updateOne({
+    "_id": "5e91d387edfdbb061454b417"
+}, {
+    "title": "修改后的title1"
+}, (err, doc) => {
     if (err) {
-        console.log("查询出错：" + err);
+        console.log(err);
+    }
+    console.log("Success!");
+});
+
+News.find({}, (err, doc) => {
+    if (err) {
+        console.log(err);
     } else {
         console.log(doc);
+    }
+}); */
+
+
+// 8. 删除数据
+console.log("------delete------");
+// 参数一为要删除的数据的查询条件
+News.deleteOne({
+    "title": "修改后的title1"
+}, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("删除成功！");
     }
 });
