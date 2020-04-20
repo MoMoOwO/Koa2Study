aa47ed5fb4c838e442bdae01a941675a807584ea
MoMoOwO <strangersj@outlook.com>
2020-04-20 22:25:34 +0800

    cms系统

--- koa打造CMS内容管理系统/koa2cms/app.js
+++ koa打造CMS内容管理系统/koa2cms/app.js
@@ -1,13 +1,25 @@
 // 引入模块
 const Koa = require("koa");
 const router = require("koa-router")();
-
+const path = require("path");
+const render = require("koa-art-template");
+const static = require("koa-static");
+//  路由模块
 const index = require("./routes/index");
 const admin = require("./routes/admin");
 const api = require("./routes/api");
 
 // 实例化一个 app
 const app = new Koa();
+
+// 配置模板引擎
+render(app, {
+    root: path.join(__dirname, "views"),
+    extname: ".html",
+    debug: process.env.NODE_ENV !== "production"
+});
+
+app.use(static(path.join(__dirname, "public")));
 
 // 配置路由
 router.use("/admin", admin);
