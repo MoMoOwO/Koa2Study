ac933b55cc77f8611d270492455c273da293aa3f
MoMoOwO <strangersj@outlook.com>
2020-04-20 22:25:05 +0800

    静态资源

--- koa打造CMS内容管理系统/koa2cms/routes/admin.js
+++ koa打造CMS内容管理系统/koa2cms/routes/admin.js
@@ -1,7 +1,21 @@
 const router = require("koa-router")();
+// 子路由模块
+const login = require("./admin/login");
+const user = require("./admin/user");
+
+// 配置中间件获取 URL 地址
+router.use(async (ctx, next) => {
+    //console.log(ctx.request.header.host);
+    // 模板引擎配置全局的变量
+    ctx.state.__HOST__ = "http://" + ctx.request.header.host;
+    await next();
+});
 
 router.get("/", async (ctx) => {
     ctx.body = "后台管理";
 });
+
+router.use("/login", login);
+router.use("/user", user);
 
 module.exports = router.routes();
